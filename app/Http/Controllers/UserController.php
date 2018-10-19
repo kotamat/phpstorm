505087ea0ffb0ffa756172a5dbd19cfcac92d9fc
kotamat <kota1861@gmail.com>
2018-10-19 11:01:16 +0900

    delete unused new lines

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,21 +4,12 @@ namespace App\Http\Controllers;
 
 class UserController extends Controller
 {
-
-
-
-
-
     public function get()
     {
         /** @var \App\User $user1 */
         $user1 = \App\User::first();
 
         $user1->getKey();
-
-
-
-
 
         $user2 = \App\User::first();
         assert($user2 instanceof \App\User);
